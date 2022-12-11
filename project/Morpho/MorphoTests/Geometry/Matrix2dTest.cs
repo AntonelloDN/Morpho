a93d4f93de60e00d25d279d8774fe01eee3f3c50
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2022-12-11 17:10:52 +0100

    feat(doc): test doc for now

---
+++ project/Morpho/MorphoTests/Geometry/Matrix2dTest.cs
@@ -0,0 +1,21 @@
+﻿using Morpho25.Geometry;
+using NUnit.Framework;
+using System.Linq;
+
+namespace MorphoTests.Geometry
+{
+    public class Matrix2dTest
+    {
+        [Test]
+        [TestCase("000000")]
+        public void CreateEquidistantGridTest(string defValue = null)
+        {
+            var matrix = new Matrix2d(10, 10, defValue);
+            matrix[1, 1] = "0100SD";
+         
+            Assert.That(matrix[1, 1], Is.EqualTo("0100SD"));
+            Assert.That(matrix.GetLengthY(), Is.EqualTo(10));
+            Assert.That(matrix.GetLengthX(), Is.EqualTo(10));
+        }
+    }
+}
